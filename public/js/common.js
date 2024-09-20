bc8bcc1650ff595b7e0b6dd298fc723e52afd5b7
youzhen <yl301114@gmail.com>
2024-09-20 22:03:24 +0100

    template finish

--- public/js/common.js
+++ public/js/common.js
@@ -19,32 +19,6 @@ function resize() {
         $(".header_lang").removeClass("active");
     }
 
-    
-    let serviceTop = $("#service").offset().top - $(".header").height();
-    let worksTop = $("#works").offset().top - $(".header").height();
-    let aboutTop = $("#about").offset().top - $(".header").height();
-    let contactTop = $("#contact").offset().top - $(".header").height();
-    let contactBottom = $("#contact").offset().top - $(window).height();
-
-    // sidebtn
-    if($(window).scrollTop() >= contactBottom) {
-        $(".sidebtn").addClass("bluebg");
-    }else {
-        $(".sidebtn").removeClass("bluebg");
-    }
-
-    // navbar
-    if($(window).scrollTop()>=serviceTop && $(window).scrollTop()<worksTop){
-        $(".gotoelement-service").addClass("active").parents("li").siblings("li").find(".gotoelement").removeClass("active");
-    }else if($(window).scrollTop()>=worksTop && $(window).scrollTop()<aboutTop) {
-        $(".gotoelement-works").addClass("active").parents("li").siblings("li").find(".gotoelement").removeClass("active");
-    }else if($(window).scrollTop()>=aboutTop && $(window).scrollTop()<contactTop) {
-        $(".gotoelement-about").addClass("active").parents("li").siblings("li").find(".gotoelement").removeClass("active");
-    }else if($(window).scrollTop()>=contactTop) {
-        $(".gotoelement-contact").addClass("active").parents("li").siblings("li").find(".gotoelement").removeClass("active");
-    }else {
-        $(".gotoelement").removeClass("active");
-    }
 }
 
 $(".header_menu").click(function() {
@@ -91,16 +65,5 @@ $(".btn-top").click(function() {
     $("html, body").animate({scrollTop:0},300);
 })
 
-$(".gotoelement").click(function(){
-    let target = $(this).data("target");
-    let top = $(target).offset().top - 60;
-    $("html, body").animate({scrollTop:top},300);
-    $(this).addClass("active");
-    
-    $(".header_menu").removeClass("active");
-    $(".header_nav").removeClass("active");
-    if ($(".header").hasClass("navshow")) {
-        $(".header").removeClass("navshow");
-    }
-})
+
 
